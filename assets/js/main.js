2218bb7d0c0c430454ab32c37e59c29b3d927ac3
TanThinhHung <ngocsanglx15@gmail.com>
2022-07-14 22:59:54 +0700

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,42 @@ nav__mobile.addEventListener('click', function() {
 //         console.log(e.target);
 //     }
 // })
+
+
+
+
+//hàm counter up
+const counter__number = document.querySelectorAll('.counter__number-item');
+console.log(counter__number);
+// const client = document.querySelector('.client');
+// const winner = document.querySelector('.winner');
+// const coffee = document.querySelector('.coffee');
+// const member = document.querySelector('.member');
+
+function count(el) {
+    var numberEl = el.querySelector('.counter__number-number');
+    console.log(numberEl);
+    var to = parseInt(numberEl.innerText);
+    console.log(typeof to);
+    var count = 0;
+    var time = 10;
+    var step = to / time;
+    const counting = setInterval(() => {
+        count += step;
+        if (count > to) {
+            clearInterval(counting);
+            numberEl.innerText = to;
+        } else {
+            numberEl.innerText = Math.round(count);
+        }
+    }, 100);
+
+}
+counter__number.forEach(item => {
+        count(item);
+    })
+    // count(counter__number);
+    // count(client, 123);
+    // count(winner, 300);
+    // count(coffee, 150);
+    // count(member, 1000);
